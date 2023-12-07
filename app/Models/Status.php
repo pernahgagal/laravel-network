a3c28d893ee47a2c18dd2e1560a3f733e1a09f9f
Rahmat Fauzi <madfizh@gmail.com>
2023-12-07 23:38:31 +0700

    Refactoring N+1 problem in Status Model

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -10,6 +10,8 @@ class Status extends Model
     use HasFactory;
     protected $fillable = ['body', 'identifier'];
 
+    protected $with = ['user'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
